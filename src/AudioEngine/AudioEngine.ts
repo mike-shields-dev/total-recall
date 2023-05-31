33c19db329635b870e5337542b4ada360e2286a4
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-05-31 14:32:29 +0100

    fix: The last pad pressed before failed user attempt did not sound, cleared active notes array to fix

--- src/AudioEngine/AudioEngine.ts
+++ src/AudioEngine/AudioEngine.ts
@@ -1,19 +1,29 @@
 import { Transport, Sequence } from "tone";
 import { noteNames, BPM, errorNoteIndex } from "../globals";
-import PubSub from 'pubsub-js';
-import { SEQUENCE_PLAY, SEQUENCE_ENDED, ACTIVE_PAD_INDEX, NOTE_ON, NOTE_OFF, SEQUENCE_STARTED, FAILED_ATTEMPT } from "./PubSubNameSpace";
+import PubSub from "pubsub-js";
+import {
+  SEQUENCE_PLAY,
+  SEQUENCE_ENDED,
+  ACTIVE_PAD_INDEX,
+  NOTE_ON,
+  NOTE_OFF,
+  SEQUENCE_STARTED,
+  FAILED_ATTEMPT,
+} from "./PubSubNameSpace";
 import synth from "./Synth";
 
 PubSub.subscribe(SEQUENCE_PLAY, (_, sequence) => playSequence(sequence));
 PubSub.subscribe(NOTE_ON, (_, noteIndex) => noteOn(noteIndex));
 PubSub.subscribe(NOTE_OFF, (_, noteIndex) => noteOff(noteIndex));
-PubSub.subscribe(FAILED_ATTEMPT, (_, userSequence) => playFailedAttemptAlert(userSequence));
+PubSub.subscribe(FAILED_ATTEMPT, (_, userSequence) =>
+  playFailedAttemptAlert(userSequence)
+);
 
 let activeNotes: number[] = [];
 
 function noteOn(noteIndex: number) {
   if (activeNotes.includes(noteIndex)) return;
-  
+
   synth.triggerAttack(noteNames[noteIndex]);
   activeNotes.push(noteIndex);
 }
@@ -36,44 +46,46 @@ function playNote(noteName: string, durationMs: number, time: number) {
 
 function playSequence(noteIndexSequence: number[]) {
   resetTransport();
-  Transport.set({bpm: BPM});
-  PubSub.publish(SEQUENCE_STARTED)
+  Transport.set({ bpm: BPM });
+  PubSub.publish(SEQUENCE_STARTED);
 
   const durationSecs = 0.3 * (60 / BPM);
   const durationMSecs = 1000 * durationSecs;
 
-  const noteSequence = new Sequence((time, noteIndex) => {
-    const isSequenceComplete = noteIndex < 0;
-    if(isSequenceComplete) return PubSub.publish(SEQUENCE_ENDED)
-    
-    playNote(noteNames[noteIndex], durationSecs, time);
-    PubSub.publish(ACTIVE_PAD_INDEX, noteIndex);
-    
-    setTimeout(() => PubSub.publish(ACTIVE_PAD_INDEX, -1), durationMSecs)
-  }, [...noteIndexSequence, -1]).start(0);
-  
+  const noteSequence = new Sequence(
+    (time, noteIndex) => {
+      const isSequenceComplete = noteIndex < 0;
+      if (isSequenceComplete) return PubSub.publish(SEQUENCE_ENDED);
+
+      playNote(noteNames[noteIndex], durationSecs, time);
+      PubSub.publish(ACTIVE_PAD_INDEX, noteIndex);
+
+      setTimeout(() => PubSub.publish(ACTIVE_PAD_INDEX, -1), durationMSecs);
+    },
+    [...noteIndexSequence, -1]
+  ).start(0);
+
   noteSequence.loop = false;
-  
+
   Transport.start();
 }
 
 function playFailedAttemptAlert(userSequence: number[]) {
   const bpm = 20;
   synth.triggerRelease(0);
-
+  activeNotes = [];
   resetTransport();
   Transport.set({ bpm });
-  PubSub.publish(SEQUENCE_STARTED);
 
   const durationSecs = 0.5 * (60 / bpm);
 
   const noteSequence = new Sequence(
     (time, noteIndex) => {
       const isSequenceEnded = noteIndex < 0;
-      
-      if(isSequenceEnded) {
+
+      if (isSequenceEnded) {
         PubSub.publish(SEQUENCE_ENDED);
-        PubSub.publish(ACTIVE_PAD_INDEX, -1)
+        PubSub.publish(ACTIVE_PAD_INDEX, -1);
         return;
       }
 
@@ -85,6 +97,7 @@ function playFailedAttemptAlert(userSequence: number[]) {
   ).start(0);
 
   noteSequence.loop = false;
-  
+
   Transport.start();
+  PubSub.publish(SEQUENCE_STARTED);
 }
